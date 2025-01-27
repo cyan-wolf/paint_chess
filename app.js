545c931c20e283e3156ed83fc9c3404c9f2a3e2a
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-01-27 13:58:14 -0400

    worked on server and client + added basic routes

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import http from "node:http";
+import path from "node:path";
+
+const app = express();
+const server = http.createServer(app);
+
+const __dirname = import.meta.dirname;
+
+app.use('/public', express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "client/index.html"));
+});
+
+app.get('/how-to-play', (req, res) => {
+    res.sendFile(path.join(__dirname, "client/how-to-play.html"));
+});
+
+app.get('/find-game', (req, res) => {
+    res.sendFile(path.join(__dirname, "client/find-game.html"));
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`App running at http://localhost:${port}.`);
+});
