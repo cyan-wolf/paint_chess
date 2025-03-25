0ae79ae18dcbf8e90ea3152465badb212c7d54e6
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-03-25 15:41:16 -0400

    made the DB try connecting with multiple hosts

--- server/db_conn.ts
+++ server/db_conn.ts
@@ -8,31 +8,64 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.34.0/mod.ts";
 
 import assert from "node:assert";
 
-const client = new MongoClient();
+type Error = { message: string };
 
-// Assert that environment variables are present.
-assert(process.env.ATLAS_HOST !== undefined);
-assert(process.env.ATLAS_USERNAME !== undefined);
-assert(process.env.ATLAS_PASSWORD !== undefined);
+function isError(error: unknown): error is Error {
+    return typeof(error) === "object" && error !== null && 'message' in error;
+}
 
-await client.connect({
-    db: "paint_chess",
-    tls: true,
-    servers: [
-        {
-            host: process.env.ATLAS_HOST,
-            port: 27017,
-        },
-    ],
-    credential: {
-        username: process.env.ATLAS_USERNAME,
-        password: process.env.ATLAS_PASSWORD,
+async function connect(client: MongoClient) {
+    // Assert that environment variables are present.
+    assert(process.env.ATLAS_HOSTS !== undefined);
+
+    // Get a list of possile costs.
+    const hosts = process.env.ATLAS_HOSTS.split(";");
+    
+    for (let i = 0; i < hosts.length; i++) {
+        try {
+            await connectToHost(client, hosts[i]);
+
+            console.log(`LOG: Connected to host #${i}.`);
+            break;
+        }
+        catch (error: unknown) {
+            if (isError(error)) {
+                if (error.message.includes("Could not find a master node")) {
+                    console.log(`LOG: Could not connect to host #${i}.`);
+                }
+                else {
+                    console.log(`ERROR: ${error.message}`);
+                }
+            }
+        }
+    }
+}
+
+async function connectToHost(client: MongoClient, host: string) {
+    // Assert that environment variables are present.
+
+    assert(process.env.ATLAS_USERNAME !== undefined);
+    assert(process.env.ATLAS_PASSWORD !== undefined);
+
+    await client.connect({
         db: "paint_chess",
-        mechanism: "SCRAM-SHA-1",
-    },
-});
+        tls: true,
+        servers: [
+            {
+                host,
+                port: 27017,
+            },
+        ],
+        credential: {
+            username: process.env.ATLAS_USERNAME,
+            password: process.env.ATLAS_PASSWORD,
+            db: "paint_chess",
+            mechanism: "SCRAM-SHA-1",
+        },
+    });
+}
 
-async function run() {
+async function testDBConnection(client: MongoClient) {
     try {
         const db = client.database("paint_chess");
         const collections = await db.listCollectionNames();
@@ -52,7 +85,9 @@ async function run() {
     }
 }
 
-run().catch(console.dir);
+const appClient = new MongoClient();
+await connect(appClient);
+await testDBConnection(appClient);
 
-const db = client.database("paint_chess");
+const db = appClient.database("paint_chess");
 export default db;
